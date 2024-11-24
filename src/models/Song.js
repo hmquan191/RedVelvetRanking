898ed12caa8798f6d6982298b50456af5bbf96c1
HMQ <hmquan191@gmail.com>
2024-11-24 07:20:06 +0700

    cleaning code

--- src/models/Song.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const songSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    album: { type: String, required: true },
-    releaseYear: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('Song', songSchema);
--- src/routes/songs.js
+++ src/routes/songs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Song = require('../models/Song');
+// const Song = require('../models/Song');
 const fs = require('fs');
 const csv = require('csv-parser');
 const path = require('path');
